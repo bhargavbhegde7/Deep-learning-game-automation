e32ae3e3f8c1452b337786d7a95757da46aa73b4
= <=>
2017-11-19 11:44:52 +0530

    utils added

--- ann.py
+++ ann.py
@@ -1,3 +1,5 @@
+from ann_utils import make_matrix
+use_bias = 1
 class ANN:
 
     def __init__(self):
@@ -14,5 +16,17 @@ class ANN:
 
 class Layer:
 
-    def __init__(self):
-        pass
+    def __init__(self, id, layer_size, prev_layer_size):
+
+        self.id = id
+        self.n_neurons = layer_size
+        self.bias_val = 1
+
+        self.input = [0] * self.n_neurons
+
+        self.output = [0] * (self.n_neurons + use_bias)
+        self.output[0] = self.bias_val
+
+        self.error = [0] * self.n_neurons
+
+        self.weight = make_matrix(prev_layer_size + use_bias , self.n_neurons)
---
+++ ann_utils.py
@@ -0,0 +1,6 @@
+def make_matrix(N, M):
+    """
+    Make an N rows by M columns make_matrix
+
+    """
+    return [[0 for i in range(M)] for i in range(N)]
