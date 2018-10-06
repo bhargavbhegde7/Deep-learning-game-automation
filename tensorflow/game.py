4b65070c7e49d1e750e903412dac01c34c81052d
= <=>
2018-10-07 02:18:46 +0530

    finished game, automation done

--- tensorflow/game.py
+++ tensorflow/game.py
@@ -1,27 +1,54 @@
 # youtube thenewboston python game development
-# https://youtu.be/OGDJdeiuB5M?list=PL6gx4Cwl9DGAjkwJocj7vlc_mFU-4wXJq
+# https://youtu.be/pNjSyBlbl_Q?list=PL6gx4Cwl9DGAjkwJocj7vlc_mFU-4wXJq
+
 import pygame
 
 print(pygame.init())
 
+WINDOW_HEIGHT = 200
+WINDOW_WIDTH = 800
+
+PLAYER_WIDTH = 10
+PLAYER_HEIGHT = 30
+PLAYER_X_POS = 20
+PLAYER_Y_POS = 170
+PLAYER_Y_ORIGINAL_POS = 170
+PLAYER_Y_POS_CHANGE = 0
+JUMP_HEIGHT = 100
+
+POS_CHANGE_GRANULARITY = 5
+
+OBSTACLE_WIDTH = 30
+OBSTACLE_HEIGHT = 40
+OBSTACLE_Y_POS = 160
+OBSTACLE_X_POS = WINDOW_WIDTH
+OBSTACLE_X_POS_CHANGE = 5
+
+obstaclePositions = []
+
+screenshotCount = 0
+iterationCOunt = 0
+gameExit = False
+FRAME_RATE = 60
+
 white = (255,255,255)
 black = (0, 0, 0)
 
-gameDisplay = pygame.display.set_mode((800,600))
+gameDisplay = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
 pygame.display.set_caption('jumper')
-windowSurface = pygame.display.set_mode((800, 600), 0, 32)
+windowSurface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), 0, 32)
 
 pygame.display.update()
 
-gameExit = False
-
-player_x_pos = 300
-player_y_pos = 300
-player_y_pos_change = 0
-
 clock = pygame.time.Clock()
 
-count = 0
+def drawObstacles():
+	for obstacleXPos in obstaclePositions:
+		pygame.draw.rect(gameDisplay, black, [obstacleXPos,OBSTACLE_Y_POS, OBSTACLE_WIDTH, OBSTACLE_HEIGHT])
+
+def addRandomObstacle():
+	obstaclePositions.append(WINDOW_WIDTH)
+	
 while not gameExit:
 
 	# event loop
@@ -29,33 +56,63 @@ while not gameExit:
 		if event.type == pygame.QUIT:
 			gameExit = True
 		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_SPACE:
-			
-				# make the player start going up only if already not doing so
-				if player_y_pos_change == 0:
-					player_y_pos_change = -5
+			if event.key == pygame.K_q:
+				gameExit = True
+			if event.key == pygame.K_SPACE:			
+				# make the player start going up only if it is stationary
+				if PLAYER_Y_POS_CHANGE == 0:
+					PLAYER_Y_POS_CHANGE = -1*POS_CHANGE_GRANULARITY
 
 	# when you have jumped high enough come back
-	if player_y_pos == 200:
-		player_y_pos_change = 5
+	if PLAYER_Y_POS == WINDOW_HEIGHT - JUMP_HEIGHT:
+		PLAYER_Y_POS_CHANGE = POS_CHANGE_GRANULARITY
 	
-	# player_y_pos_change > 0 means that the player is coming down
-	if player_y_pos_change > 0 and player_y_pos == 300:
-		player_y_pos_change = 0
+	# PLAYER_Y_POS_CHANGE > 0 means that the player is coming down
+	if PLAYER_Y_POS_CHANGE > 0 and PLAYER_Y_POS == PLAYER_Y_ORIGINAL_POS:
+		PLAYER_Y_POS_CHANGE = 0
 	
-	# screen shot
-	if player_y_pos_change == 0:
-	
-		pygame.image.save(windowSurface, "screenshots/screenshot_"+str(count)+".jpeg")
-		count += 1
+	# screen shot only when the player is stationary
+	if PLAYER_Y_POS_CHANGE == 0:
+		pygame.image.save(windowSurface, "screenshots/screenshot_"+str(screenshotCount)+".jpeg")
+		screenshotCount += 1
 
-	player_y_pos += player_y_pos_change
+	# update the position of player
+	PLAYER_Y_POS += PLAYER_Y_POS_CHANGE
+	
+	# update the position of the each obstacle
+	for index, position in enumerate(obstaclePositions):
+		obstaclePositions[index] -= OBSTACLE_X_POS_CHANGE
+	
+	# remove the passed obstacle from the obstacle list
+	for index, obstacleXPos in enumerate(obstaclePositions):
+		if obstacleXPos < 0:
+			obstaclePositions.pop(index)
+		
+	# add an obstacle every 100th iteration
+	if iterationCOunt % 100 == 0:
+		addRandomObstacle()	
 
+	# check if an obstacle collided with the player 
+	for obstacleXPos in obstaclePositions:
+		# check if obstacle has crossed into the player's territory
+		if obstacleXPos < PLAYER_X_POS+PLAYER_WIDTH:
+			# check if player is blocking
+			if OBSTACLE_Y_POS <= PLAYER_Y_POS:
+				gameExit = True
+	
+	# automate jump
+	# check for an obstacle nearing the player
+	for obstacleXPos in obstaclePositions:
+		if obstacleXPos <= PLAYER_X_POS+100:
+			PLAYER_Y_POS_CHANGE = -1*POS_CHANGE_GRANULARITY
+	
 	gameDisplay.fill(white)
-	pygame.draw.rect(gameDisplay, black, [player_x_pos,player_y_pos, 10, 30])
+	pygame.draw.rect(gameDisplay, black, [PLAYER_X_POS,PLAYER_Y_POS, PLAYER_WIDTH, PLAYER_HEIGHT])	
+	drawObstacles()
 	pygame.display.update()
+	iterationCOunt += 1
 
-	clock.tick(30)
+	clock.tick(FRAME_RATE)
 
 pygame.quit()
 quit()
